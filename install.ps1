f0e175e96d140dbbc2688f335db2a5bc62183b20
Jialin-Eddie <zhaojialin698@gmail.com>
2024-10-20 18:01:28 +0800

    newest verion and next step is analysis

--- install.ps1
+++ install.ps1
@@ -1,5 +1,9 @@
-ECHO ���ڴ�״̬��
-
-cd to workspace
-.\.venv\Scripts\activate
+# Set up and activate virtual environment
+# cd to portfolio managment first or equal directory with install.ps1
+Write-Host "Setting up virtual environment..."
+python -m venv .venv
+Write-Host "Activating virtual environment..."
+.venv\Scripts\Activate.ps1
+Write-Host "Installing dependencies..."
 pip install -r requirements.txt
+Write-Host "Setup complete. Ready to use!"
